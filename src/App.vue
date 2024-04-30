dba07a8f5c548f6cfc0a1b7aef13dbf287a61b50
tbb <franktang196@gmail.com>
2024-04-30 15:07:20 +0800

    调整导航栏样式及适配

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
     <h1 class="main-title">武汉房地产与金融</h1>
     </div>
     <nav class="nav-container">
-      <router-link to="/newhouse" class="nav" :class="{ active: $route.path === '/newhouse' }">新房</router-link>
-      <router-link to="/secondhand" class="nav" :class="{ active: $route.path === '/secondhand' }">二手房</router-link>
-      <router-link to="/socialfinancing" class="nav" :class="{ active: $route.path === '/socialfinancing' }">社会融资增量</router-link>
-      <router-link to="/currencyissue" class="nav" :class="{ active: $route.path === '/currencyissue' }">货币供应量M2</router-link>
+      <router-link to="/newhouse" class="nav" :class="{ active: $route.path === '/newhouse' }">武汉新房</router-link>
+      <router-link to="/secondhand" class="nav" :class="{ active: $route.path === '/secondhand' }">武汉二手房</router-link>
+      <router-link to="/socialfinancing" class="nav" :class="{ active: $route.path === '/socialfinancing' }">社资增量</router-link>
+      <router-link to="/currencyissue" class="nav" :class="{ active: $route.path === '/currencyissue' }">货币M2</router-link>
       <router-link to="/populationcorrelation" class="nav" :class="{ active: $route.path === '/populationcorrelation' }">人口数据</router-link>
       <router-link to="/priceindices" class="nav" :class="{ active: $route.path === '/priceindices' }">价格指数</router-link>
     </nav>
@@ -31,25 +31,33 @@ export default {
 }
 .nav-container {
   overflow-x: auto; /* 启用水平滚动条 */
-  white-space: nowrap; /* 防止换行 */
+  /** white-space: nowrap; /* 防止换行 */
+  white-space: wrap; /* 换行：移动端换行显示 */
+  flex-wrap: wrap;
   -webkit-overflow-scrolling: touch; /* 在移动设备上优化滚动 */
   display: flex;
   justify-content: safe center; /* 解决居中滚动时左边无法显示全的问题 */
   margin-top: 20px; /* 添加底部间距 */
   background-color: #fff; /* 设置背景颜色为灰色 */
+  border-radius: 40px;
+  box-shadow: 0 5px 10px rgb(0, 0, 0, 0.25);
+  
 }
 
 .nav {
+  display:block;
   text-decoration: none;
-  background-color: #ccc;
-  color: #fff; /* 设置文字颜色为黑色 */
-  padding: 10px 20px;
-  /* border-radius: 5px; */
-  transition: background-color 0.3s ease;
+  background-color: #fff;
+  color: #000; /* 设置文字颜色为黑色 */
+  padding: 10px 10px;
+  margin: 10px 10px;
+  font-weight: bold;
+  border-radius: 40px;
 }
 
 .nav.active {
   color: #fff !important; /* 使用 !important 提高优先级 */
   background-color: #0bc2d6 !important; /* 使用 !important 提高优先级 */
 }
+
 </style>
