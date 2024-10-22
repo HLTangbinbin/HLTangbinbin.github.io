31b825513d9b7c55696de32749e3c23c250098c4
tbb <franktang196@gmail.com>
2024-10-22 09:01:26 +0800

    优化移动设备上公积金图片显示

--- src/components/NationWide/ProvidentFund.vue
+++ src/components/NationWide/ProvidentFund.vue
@@ -18,7 +18,7 @@
   export default {
     data() {
       return {
-        imageSrc: '/images/providentFund.jpg', // 图片路径
+        imageSrc: './images/providentFund.jpg', // 图片路径
         imgWidth: 0, // 原始图片宽度
         imgHeight: 0, // 原始图片高度
         currentScale: 1, // 实时缩放比例
@@ -55,14 +55,14 @@
         this.updateImageStyle(); // 初始化样式
       },
       updateImageStyle() {
-        const scaledWidth = this.imgWidth * 1; // 初始宽度为原始宽度的 1 倍
-        const scaledHeight = this.imgHeight * 1; // 初始高度为原始高度的  倍
+        const scaledWidth = this.imgWidth * this.currentScale;
+        const scaledHeight = this.imgHeight * this.currentScale;
   
         // 根据当前缩放比例更新图片样式
         this.imageStyle = {
           width: `${scaledWidth}px`,
           height: `${scaledHeight}px`,
-          transform: `scale(${this.currentScale})`, // 应用当前缩放比例
+          transform: `scale(1)`, // 应用当前缩放比例
           transformOrigin: 'center center', // 从左上角缩放
         };
       },
@@ -71,27 +71,30 @@
   </script>
   
   <style scoped>
-  #image-container {
-    display: flex;
-    justify-content: center;
-    align-items: flex-start; /* 垂直对齐方式改为顶部对齐 */
-    width: 100%;
-    height: 100%; /* 高度减去顶部留白 */
-    padding-top: 50px;
-    padding-bottom: 50px;
-    overflow: hidden; /* 防止图片溢出 */
-    touch-action: none; /* 禁用浏览器默认手势 */
-  }
-  
-  #image-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: flex-start; /* 垂直对齐方式改为顶部对齐 */
-  }
-  
-  img {
-    max-width: none; /* 防止图片被限制 */
-    max-height: none;
-    will-change: transform; /* 优化性能 */
-  }
+    #image-container {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        width: 100%;
+        height: 100%;
+        padding-top: 50px;
+        padding-bottom: 50px;
+        overflow: hidden;
+        touch-action: none;
+    }
+
+    /* 大屏幕设置 */
+    @media (min-width: 769px) {
+        img {
+            max-width: 100%; /* 确保图片在大屏幕上撑满容器 */
+        }
+    }
+
+    /* 移动设备设置 */
+    @media (max-width: 768px) {
+        img {
+            max-width: 100%; /* 移动设备图片保持原始大小 */
+            height: auto;    /* 自动调整高度，保持图片比例 */
+        }
+    }
   </style>
