26c7796c9810b9d14fed6f4be3a53d612b03a536
tbb <bbt@192.168.1.16>
2025-08-10 10:14:40 +0800

    Test GitHub Actions auto deploy

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 使用该配置打包到github pages后容易出现空白页面
-  // publicPath: process.env.NODE_ENV === 'production' ? '/HLTangbinbin.github.io/' : './'
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/HLTangbinbin.github.io/' : '/',
+  // publicPath: './',
   devServer: {
     proxy: {
       '/api': {
